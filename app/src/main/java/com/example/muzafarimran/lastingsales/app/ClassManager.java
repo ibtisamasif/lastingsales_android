33c4245cd1eaa445596a1b97cd726b673ac69307
4540S\ibtisam <ibtisam.asif@vividtech.io>
2017-11-21 11:54:06 +0500

    toota hua code part 2 return

---
+++ app/src/main/java/com/example/muzafarimran/lastingsales/app/ClassManager.java
@@ -0,0 +1,8 @@
+package com.example.muzafarimran.lastingsales.app;
+
+/**
+ * Created by ibtisam on 11/20/2017.
+ */
+
+class ClassManager {
+}
---
+++ app/src/main/java/com/example/muzafarimran/lastingsales/recycleradapter/ExampleAdapter.java
@@ -0,0 +1,8 @@
+package com.example.muzafarimran.lastingsales.recycleradapter;
+
+/**
+ * Created by ibtisam on 11/20/2017.
+ */
+
+class ExampleAdapter {
+}
